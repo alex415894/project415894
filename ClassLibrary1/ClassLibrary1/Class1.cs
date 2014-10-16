6bff94fd9777536e3fe0585ca387aa8fb9d8cc82
alex415894 <alex415894@gmail.com>
2014-10-16 23:26:18 +0300

    библиотека

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibrary48
+{
+       
+    public class Class48
+     {
+        public void Show ()
+         {
+             MessageBox.Show("Привет");
+         }
+        
+    }
+}
