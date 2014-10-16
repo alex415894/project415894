d751a08b76412fd7c56aac7a27e690e02386dd4d
alex415894 <alex415894@gmail.com>
2014-10-16 22:03:50 +0300

    просто

---
+++ Бинарный калькулятор/ConsoleApplication21/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static void Main()
+        {
+            Core_calculator Proba = new Core_calculator();
+            Core_calculator_brackets Proba_2 = new Core_calculator_brackets();
+            CCB_MaztFun Proba_3 = new CCB_MaztFun();
+
+            Console.WriteLine("Сделайте выбор калькулятора");
+            Console.WriteLine("Введите '1' или '2' или '3'");
+            string V = Console.ReadLine();
+            switch (V)
+            {
+                case "1":
+                    Proba.Symbol();
+                    break;
+                case "2":
+                    Proba_2.Symbol();
+                    break;
+                case "3":
+                    Proba_3.Symbol();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка ввода");
+                    break;
+
+            }
+
+                              
+            Console.ReadLine();
+        }                      
+    }                         
+}  
+                           
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
